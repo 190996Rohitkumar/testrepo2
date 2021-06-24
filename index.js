3fb64460ae6cbe1a89a2a2532322fab7ab70e822
190996Rohitkumar <rohitsharma82365@gmail.com>
2021-06-24 20:50:13 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+ const http=require("http");
+ http.createServer(function(req,res){
+     res.writeHead(200,{"content-Type":"text/html"});
+     res.write("<h1>Hello</h1>");
+     res.end();
+     
+ }).listen(8100);
+ console.log("the server is running on port");
