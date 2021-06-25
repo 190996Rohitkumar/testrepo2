cd54a03155a191ce592a45389c7488df5a670ea1
190996Rohitkumar <rohitsharma82365@gmail.com>
2021-06-25 11:35:50 +0530

    fifth commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
  const http=require("http");
  http.createServer(function(req,res){
      res.writeHead(200,{"content-Type":"text/html"});
-     res.write("<h1>Hello Wordl</h1>");
+     res.write("<h1>Hello world This is Rohit kashyap sharma</h1>");
      res.end();
      
  }).listen(process.env.PORT || 5100);
